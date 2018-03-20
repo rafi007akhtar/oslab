389da972bbfba27dd37d6fa08906caa14b70553f
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-03-20 18:47:29 +0530

    First part done - yay!

--- Simple-UNIX-shell/shell.c
+++ Simple-UNIX-shell/shell.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> // includes fork(), execvp()
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_LINE		80 /* 80 chars per line, per command */
 
-char *hints = "\nWelcome to Guest session. Non-paramter commands are suppported in this basic shell (eg., ls, ps, pwd, whoami, gedit, vi, and so on). \nNote that commands that require one or more parameters are unsupported for now (like cd, mkdir, and so on). \nTo view these instructions anytime, press 'hints'. \nTo exit anytime, press 'quit'. \n";
+char *hints = "\nWelcome to Guest session. Most terminal commands are supported in this shell. \n\nNote that you cannot change directories with the guest session in this shell, meaning the command 'cd <dest>' is not supported so long as you are a guest user. \nIn order to be able to change directories and much more, change into root user by entering 'sudo -i'. \nYou can shift back to guest session by typing 'exit' when root. \nYou can see what user you are by entering 'whoami'. \n\nTo view these instructions anytime, press 'hints'. \n\nTo exit anytime, press 'quit'. \n";
 
-int hCount = 0;
-char *history[10];
+//int hCount = 0;
+//char *history[10];
+
+typedef enum boolean {false, true} bool;
 
 void printArray(char **params, int l)
 {
@@ -21,35 +24,41 @@ int main(void)
 	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
 	char *params[39]; // parameters
 	
-    int should_run = 1;
+    bool should_run = true;
+    bool waitFlag;
 	int i, j;
 	
 	pid_t pid;
 	char temp[80];
 	char *words;
 	
-	printf(hints);
+	printf("%s", hints);
 		
-    while (should_run){   
+    while (should_run)
+    {   
         printf("\nguest> ");
         fflush(stdout);
         
+        waitFlag = true; // By default, wait for a process to finish execution of its child
+        
         // take the string input, and split the arguments into words
         gets(temp);
-        history[hCount++] = temp;
-        if (!strcmp(temp, "hints")) printf(hints);
-        if (!strcmp(temp, "quit")) should_run = 0;
-        if (!strcmp(temp, "history")) printf("%s", history[hCount-1]);
+        //history[hCount++] = temp;
+        if (!strcmp(temp, "hints")) printf("%s", hints);
+        if (!strcmp(temp, "quit")) should_run = false;
+        //if (!strcmp(temp, "history")) printf("%s", history[hCount-1]);
 
         words = strtok(temp, " ");
         i = j = 0;
         while(words != NULL)
         {
-        	args[i] = words;
-        	if (i != 0) { params[j] = args[i]; j++; }
+        	if (strcmp(words, "&"))
+        		args[i] = words;
+        	else { waitFlag = false; break; }
         	i++;
         	words = strtok(NULL, " ");
         }
+        args[i] = NULL;
         
         //printArray(params, j); // Testing for the printing of correct paramters
         
@@ -66,18 +75,18 @@ int main(void)
         else if (pid == 0) // child process
         {
         	// invoke execvp()
-        	execvp(args[0], params);
+        	//printArray(params, j);
+        	execvp(args[0], args);
+        	return 0;
         }
         else // parent
         {
-        	/* REVERSNG THE QUESTION
-        	 * Default behaviour: Parent waits for child to end before continuing
-        	 * When user enters '&': Parent and child run concurrently 
-        	 */
-        	if (strcmp(params[j-1], "&"))
-        	wait(NULL); 
+        	// Wait for child to finish execution unless user ends with '&' (reversing the original part of the problem)
+        	if (waitFlag == true)	wait (NULL);
+        	else waitFlag = true;
         }
     }
     
 	return 0;
 }
+
