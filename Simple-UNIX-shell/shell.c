57909a2de3e991cb0832121944e47509f02bf901
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-01 18:15:44 +0530

    Documented the code

--- Simple-UNIX-shell/shell.c
+++ Simple-UNIX-shell/shell.c
@@ -1,31 +1,38 @@
+/** Emulate a basic Linux terminal, and some features of your own */
+
 #include <stdio.h>
 #include <unistd.h> // includes fork(), execvp()
 #include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
+#include <ctype.h> // includes atoi
+#include <sys/types.h> // include pid_t
 #include <sys/wait.h> // includes wait()
 #include <malloc.h>
 #include <stdlib.h> // contains atoi(), exit()
 
 #define MAX_LINE		80 /* 80 chars per line, per command */
 
-char *hints = "\nWelcome to this session, user. Most terminal commands are supported in this shell. \n\nApart from them, this shell supports a few other commands. Enter 'extras' to know what they are. \nYou can see what user you are by entering 'whoami'. \n\nTo view these instructions anytime, press 'hints'. \n\nTo exit anytime, enter 'quit'. \n\nⓒ  Md Rafi Akhtar, 2018\n";
+char *hints = "\nWelcome to this session, user. Most terminal commands are supported in this shell. \n\nApart from them, this shell supports a few other commands. Enter 'extras' to know what they are. \nYou can see what user you are by entering 'whoami'. \n\nTo view these instructions anytime, press 'hints'. \n\nTo exit the current process and move to immediate parent process, enter 'quit' \nNOTE: Entering 'quit' in the parent process will end this shell process \n\nTo exit anytime, press Ctrl+C or Ctrl+Z. \n\nⓒ  Md Rafi Akhtar, 2018\n";
 
-char *extras[] = {{"\n1. <command> & \n Apply '&' at the end of a shell command so that the parent process doesn't wait for the child to terminate and run concurrently with it. \n Eg: ps -ael & \n Warning: This normally results in segmentation fault, hence it is heavily ill-advised to use this command. \n"}, {"\n2. hints \n Shows startup hints on the console. \n"}, {"\n3. quit \n Quits the shell when in user-mode. \n If you are in root mode, you first need to return back to user mode by entering 'exit', and then enter 'quit'. \n"}};
-const int extraItems = 3;
+char *extras[] = {{"\n1. <command> & \n Apply '&' at the end of a shell command so that the parent process doesn't wait for the child to terminate and run concurrently with it. \n Eg: ps -ael & \n Warning: This normally results in segmentation fault, hence it is heavily ill-advised to use this command. \n"}, {"\n2. hints \n Shows startup hints on the console. \n"}, {"\n3. !! \n Run the most recent command. \n"}, {"\n4. !<someInt> \n Run the nth command in history \n Eg., !5 will run the 5th command in history (provided there is one). \n"}, {"\n5. history \n Show the most recent commands in history, with most recent at the bottom \n"}, {"\n6. quit \n Quits the shell when in user-mode. \n If you are in root mode, you first need to return back to user mode by entering 'exit', and then enter 'quit'. \n"}};
+const int extraItems = 6; // NOTE: Increment this number as you increase items in extras
 
 typedef enum boolean {false, true} bool;
 
 int histItems = 0; // keeps a track of the number of commands executed
+bool waitFlag = true; // will wait for the child process to end execution until user end with '&'
 
 void printArray(char **params, int l)
 {
+	/** Print elements of the string array params of length l */
+
 	int i;
 	for (i = 0; i < l; i++) printf("%s", params[i]);
 }
 
 void updateHistory(char *temp)
 {
+	/** Add commands to the history file as user enters them */
+
 	FILE *f = fopen("history.txt", "a+");
 	fprintf(f, "%s\n", temp);
 	fclose(f);
@@ -33,6 +40,15 @@ void updateHistory(char *temp)
 
 void printHistory()
 {
+	/** Print history of commands line by line in the format:
+	 * <numberOfOccurence> <command entered>
+	 */
+
+	/* NOTE: Changing the question statement here: 
+	 * Printing all commands printed in the current session (instead of the 10 most recent)
+	 * Printing the commands 'most recent at the bottom' wise, instead of the other way round 
+	 */
+
 	char *line = NULL;
     size_t len = 0;
 	FILE *f = fopen("history.txt", "a+");
@@ -47,6 +63,8 @@ void printHistory()
 
 int execute(char *command)
 {
+	/** Execute the command entered by the user */
+
 	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
 	char *words;
 	bool waitFlag = true;
@@ -93,6 +111,8 @@ int execute(char *command)
 
 void runMostRecent()
 {
+	/** Execute the most recent command in history (by invoking !! from the user) */
+
 	char *mostRecentCommand = "";
 	char *line = NULL;
     size_t len = 0, count = 0;
@@ -114,31 +134,41 @@ void runMostRecent()
 
 int getNth(char *num)
 {
+	/** Fetch nth command in the history by invoking !n from the user, where n is the command number */
+
 	int l = strlen(num);
 	int i, j = 0, n;
 	char temp[l];
+
+	// Eliminate ! from !n to get the command number, n
 	for (i = 1; i < l; i++)
 		temp[j++] = num[i];
 	temp[j] = '\0';
 	n = atoi(temp); // atoi(str) converts str from string to int
+
 	return n; 
 }
 
 void runNth(char *num)
 {
-	int n = getNth(num);
+	/** Execute nth command in the history by invoking !n from the user, where n is the command number */
 
+	int n = getNth(num); // isolate the command number from !commandnumber
+
+	// Check for invalid input number
 	if (n > histItems || n < 1)
 	{
 		printf("Error: Command number %d does not exist\n", n);
 		return;
 	}
+
 	char *nthCommand = "";
 	char *line = NULL;
     size_t len = 0, count = 0;
 	int i = 0, getVal;
 	char trimmedCommand[80]; // containts the command devoid of '\n' at the end
 	
+	// Open history and fetch the nth command. This will contain a '\n' at the end
 	FILE *f = fopen("history.txt", "a+");
 	for (i = 0; i < n; i++)
 	{
@@ -159,7 +189,6 @@ int main()
 
 	bool should_run = true;
 	int i, getVal, n;
-	bool waitFlag = true;
 
 	pid_t pid;
 	char temp[80];
@@ -176,7 +205,7 @@ int main()
 		printf("\nuser> ");
 		fflush(stdout);
 
-		// take the string input, and perform some user-defined actions
+		// take the string input, and update history (if it's not invoking a previous command through '!')
 		gets(temp);
 
 		if (!strcmp(temp, "quit"))  { should_run = false; exit(0); }
@@ -199,9 +228,7 @@ int main()
 				continue; 
 			}
 			else 
-			{
 				getVal = execute(temp);
-			}
 			if (getVal == 1) continue;
 		}
 		else // parent
